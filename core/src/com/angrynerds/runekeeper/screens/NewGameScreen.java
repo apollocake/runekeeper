bd8778bf3ef5200aca5034f0db8cdee465c5545c
Afton Martin <aftonam@gmail.com>
2016-09-27 20:16:46 -0300

    added pressing Y goes to save menu

--- core/src/com/angrynerds/runekeeper/screens/NewGameScreen.java
+++ core/src/com/angrynerds/runekeeper/screens/NewGameScreen.java
@@ -3,6 +3,7 @@ package com.angrynerds.runekeeper.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -18,6 +19,8 @@ public class NewGameScreen extends RunekeeperScreen {
     Stage stage;
     SpriteBatch batch;
     Skin skin;
+    float time = 0;
+
     
     public NewGameScreen(Game game) {
         super(game);
@@ -50,6 +53,16 @@ public class NewGameScreen extends RunekeeperScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
+
+        time += delta;
+        if (time > 1) {
+
+            if (Gdx.input.isKeyPressed(Input.Keys.Y)) {
+                //move to a different game screen
+                game.setScreen(new SaveScreen(game));
+            }
+
+        }
     }
 
     @Override
