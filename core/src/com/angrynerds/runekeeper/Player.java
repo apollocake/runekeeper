4aad1da6ee314838b18a92842dd0dde104a8c6e6
qzhao1992 <qzhao@uco.edu>
2016-09-27 21:34:38 -0500

    Finished Attacking Animation

---
+++ core/src/com/angrynerds/runekeeper/Player.java
@@ -0,0 +1,192 @@
+
+package com.angrynerds.runekeeper;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.g2d.Animation;
+
+
+
+public class Player{
+    //DEFINED FOR THE CORRESPONDING ANIMATIONS
+    public final int IDLE = 0; 
+    public final int UP = 2;
+    public final int DOWN = -2;
+    public final int LEFT = -2;
+    public final int RIGHT = 2;
+
+    public PlayerAnimation playerAnimation;
+    AttackingFunction attackingFunction; 
+    public Animation animation;
+    
+    public String direction = "DOWN";
+    String attack = "";
+    public Vector2 pos = new Vector2();
+    
+    public Player ( float x, float y) {
+        pos.x = x;
+        pos.y = y;
+        attackingFunction = new AttackingFunction();
+        playerAnimation = new PlayerAnimation(pos.x, pos.y);
+        animation = playerAnimation.downIdling;
+    }
+    
+    public void update () {	
+        processKeys();
+        playerAnimation.setLocation(pos.x, pos.y);
+        attackingFunction.setLocation(pos.x, pos.y);
+    }
+    
+    private void processKeys () {
+        
+        if (Gdx.input.isKeyPressed(Keys.A) && Gdx.input.isKeyPressed(Keys.W)){
+            direction = "LEFT";
+            pos.x -= Math.sin(Math.toRadians(45))*2;
+            pos.y += Math.cos(Math.toRadians(45))*2;
+            animation = playerAnimation.walkingLeftAnima;
+            
+        }
+                
+        else if (Gdx.input.isKeyPressed(Keys.D) && Gdx.input.isKeyPressed(Keys.W)){
+            direction = "RIGHT";
+            pos.x += Math.sin(Math.toRadians(45))*2;
+            pos.y += Math.cos(Math.toRadians(45))*2;
+            animation = playerAnimation.walkingRightAnima;
+            animation = playerAnimation.rightIdling;
+        }
+        
+        else if (Gdx.input.isKeyPressed(Keys.A) && Gdx.input.isKeyPressed(Keys.S)){
+            direction = "LEFT";
+            pos.x -= Math.sin(Math.toRadians(45))*2;
+            pos.y -= Math.cos(Math.toRadians(45))*2;
+            animation = playerAnimation.walkingLeftAnima;
+            animation = playerAnimation.leftIdling;
+        }
+                
+        else if (Gdx.input.isKeyPressed(Keys.D) && Gdx.input.isKeyPressed(Keys.S)){
+            direction = "RIGHT";
+            pos.x += Math.sin(Math.toRadians(45))*2;
+            pos.y -= Math.cos(Math.toRadians(45))*2;
+            animation = playerAnimation.walkingRightAnima;
+            
+        }
+    
+        else if (Gdx.input.isKeyPressed(Keys.W)) {
+            direction  = "UP";
+            pos.y += UP;
+            animation = playerAnimation.walkingUpAnima;
+            
+        }	
+        else if (Gdx.input.isKeyPressed(Keys.A)) {
+            direction = "LEFT";
+            pos.x += LEFT;
+            animation = playerAnimation.walkingLeftAnima;
+           
+        } 
+        else if (Gdx.input.isKeyPressed(Keys.D)) {
+            direction  = "RIGHT";
+            pos.x += RIGHT;
+            animation = playerAnimation.walkingRightAnima;
+           
+
+        } 
+        else if(Gdx.input.isKeyPressed(Keys.S)) {
+            direction  = "DOWN";
+            pos.y += DOWN;
+            animation = playerAnimation.walkingDownAnima;
+           
+
+        }
+        
+       else if(Gdx.input.isKeyPressed(Keys.SPACE) && direction.equals("DOWN")){
+            animation = attackingFunction.attackingDownAnima;
+            attack  = "DOWN_ATTACKING";
+            
+        }
+        
+        else if(Gdx.input.isKeyPressed(Keys.SPACE) && direction.equals("UP")){
+            animation = attackingFunction.attackingUpAnima;
+            attack  = "UP_ATTACKING";
+        }
+        
+        else if(Gdx.input.isKeyPressed(Keys.SPACE) && direction.equals("LEFT")){
+            animation = attackingFunction.attackingLeftAnima;
+            attack  = "LEFT_ATTACKING";
+        }
+        
+         else if(Gdx.input.isKeyPressed(Keys.SPACE) && direction.equals("RIGHT")){
+            animation = attackingFunction.attackingRightAnima;
+           attack  = "RIGHT_ATTACKING";
+        }
+        
+//         else{
+//             if(direction.equals("LEFT")){
+//                animation = attackingFunction.leftIdling;
+//            }
+//             
+//            if(direction.equals("RIGHT")){
+//                animation = attackingFunction.rightIdling;
+//            }
+//             
+//            if(direction.equals("UP")){
+//                animation = attackingFunction.upIdling;
+//            }
+//             
+//            if(direction.equals("DOWN")){
+//                animation = attackingFunction.downIdling;
+//            }
+//         }
+        
+        
+        else{
+            //X AND Y SHOULD NOT CHANGE
+            //This is when the chatcter is in IDLE
+            if(direction.equals("LEFT")){
+                animation = playerAnimation.leftIdling;
+            }
+             
+            else if(direction.equals("RIGHT")){
+                animation = playerAnimation.rightIdling;
+            }
+             
+            else if(direction.equals("UP")){
+                animation = playerAnimation.upIdling;
+            }
+             
+            else if(direction.equals("DOWN")){
+                animation = playerAnimation.downIdling;
+            }
+            
+            //
+            else if(attack.equals("DOWN_ATTACKING")){
+                animation = attackingFunction.downIdling;
+            }
+            
+            else if(attack.equals("UP_ATTACKING")){
+                animation = attackingFunction.upIdling;
+            }
+            
+            else if(attack.equals("LEFT_ATTACKING")){
+                animation = attackingFunction.leftIdling;
+            }
+            
+            else if(attack.equals("RIGHT_ATTACKING")){
+                animation = attackingFunction.rightIdling;
+            }
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    
+}
--- core/src/com/angrynerds/runekeeper/screens/StartScreen.java
+++ core/src/com/angrynerds/runekeeper/screens/StartScreen.java
@@ -1,6 +1,7 @@
 package com.angrynerds.runekeeper.screens;
 
 import com.angrynerds.runekeeper.AttackingFunction;
+import com.angrynerds.runekeeper.Player;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -13,6 +14,7 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -21,6 +23,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import static sun.audio.AudioPlayer.player;
 
 //code based off of the demos at the bottom of this page: https://github.com/libgdx/libgdx/wiki/Scene2d.ui
 //repo here: https://github.com/libgdx/libgdx/blob/master/tests/gdx-tests/src/com/badlogic/gdx/tests/UISimpleTest.java#L37
@@ -32,7 +35,13 @@ public class StartScreen extends RunekeeperScreen {
     SpriteBatch batch;
     float time = 0;
     private Music music;
-    AttackingFunction attackingFunction = new AttackingFunction();
+    //AttackingFunction attackingFunction = new AttackingFunction();
+    
+    
+        
+    public Player player = new Player(25,25);
+    TextureRegion currentFrame;  
+    float stateTime;
 
     public StartScreen(Game game) {
         super(game);
@@ -92,13 +101,14 @@ public class StartScreen extends RunekeeperScreen {
         music = Gdx.audio.newMusic(Gdx.files.internal("startmenu.mp3"));
         music.setLooping(true);
         music.play();
-
+                
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT| GL20.GL_DEPTH_BUFFER_BIT); 
+
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
 
@@ -107,13 +117,21 @@ public class StartScreen extends RunekeeperScreen {
            // if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY) || Gdx.input.justTouched()) {
                 //move to a different game screen
                 //game.setScreen(new GameScreen(game));
-           // }
+           // } 
         }
         
         if(Gdx.input.isKeyPressed(Keys.SPACE )){
-            attackingFunction.attacking();
-            //System.out.println("I am attacking");
+            System.out.println("I am attacking");
         }
+        
+        stateTime += Gdx.graphics.getDeltaTime();  
+        currentFrame  = player.animation.getKeyFrame(stateTime, true); 
+        batch.begin();
+        batch.draw(currentFrame, player.pos.x, player.pos.y);        
+        player.update();
+        batch.end();
+        
+                
     }
 
     @Override
